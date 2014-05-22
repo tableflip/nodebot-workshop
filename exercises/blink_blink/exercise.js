247a5fb9b0fb4351252627c4a60f8e91e5bd8be7
Alan Shaw <alan@freestyle-developments.co.uk>
2014-05-22 12:56:02 +0100

    Stub out johnny five so we can assert on function calles for johnny-five objects

--- exercises/blink_blink/exercise.js
+++ exercises/blink_blink/exercise.js
@@ -1,19 +1,12 @@
-var proxyquire =  require("proxyquire"),
-  should = require('chai').should();
-
-// these will be required() in place of the real modules
-var stubs = {
-  "firmata": require('../../stubs/io-stub'),
-  "serialport": require('../../stubs/serialport-stub')
-};
-stubs['serialport']['@global'] = true;
-stubs['firmata']['@global'] = true;
+var proxyquire = require("proxyquire");
+var five = require("../../stubs/five-led");
+var expect = require("chai").expect;
 
 var exercise      = require('workshopper-exercise')(),
   filecheck       = require('workshopper-exercise/filecheck'),
   execute         = require('workshopper-exercise/execute'),
   wrappedexec     = require('workshopper-wrappedexec'),
-  path = require('path')
+  path            = require('path')
 
 // checks that the submission file actually exists
 exercise = filecheck(exercise)
@@ -28,7 +21,7 @@ exercise = wrappedexec(exercise)
 // this actually runs the solution
 exercise.addProcessor(function (mode, callback) {
   // includes the solution to run it
-  proxyquire(path.join(process.cwd(), exercise.args[0]), stubs);
+  proxyquire(path.join(process.cwd(), exercise.args[0]), {"johnny-five": five});
 
   // need a better way of detecting when we are done..
   setTimeout(function() {
@@ -39,19 +32,24 @@ exercise.addProcessor(function (mode, callback) {
 // add a processor only for 'verify' calls
 exercise.addVerifyProcessor(function (callback) {
   try {
-    var io = stubs['firmata'].singleton;
+    var io = five.stubs.firmata.singleton;
 
-    if(!io) {
+    if (!io) {
       // yikes, board was never created
       return callback(null, false);
     }
 
+    var led = five.instances.Led[0];
+
+    expect(led.pin, "led expected to be connected to pin 13").to.equal(13);
+    expect(led.strobe.calledWith(1000), "led.strobe was not called with 1000").to.be.true;
+
     // should have set pin 13 into digital output mode
-    io.pinMode.calledWith(13, io.MODES.OUTPUT).should.be.true;
+    expect(io.pinMode.calledWith(13, io.MODES.OUTPUT)).to.be.true;
 
     // should have turned pin 13 on and off
-    io.digitalWrite.calledWith(13, io.HIGH).should.be.true;
-    io.digitalWrite.calledWith(13, io.LOW).should.be.true;
+    expect(io.digitalWrite.calledWith(13, io.HIGH)).to.be.true;
+    expect(io.digitalWrite.calledWith(13, io.LOW)).to.be.true;
 
     callback(null, true)
   } catch(e) {
