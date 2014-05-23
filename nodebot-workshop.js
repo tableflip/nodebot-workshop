6236a2c9d36c3eef106ba8e51cf1ed21f16424aa
olizilla <oli@zilla.org.uk>
2014-05-23 12:26:32 +0100

    Update name in menu

--- nodebot-workshop.js
+++ nodebot-workshop.js
@@ -4,15 +4,13 @@ const workshopper = require('workshopper')
     , path        = require('path')
     , menu        = require('./exercises/menu')
 
-    , name        = 'johnny-five-is-alive'
-    , title       = 'johnny-five is ALIVE!'
+    , name        = 'nodebot-workshop'
+    , title       = 'nodebot-workshop'
     , subtitle    = '\x1b[23mSelect an exercise and hit \x1b[3mEnter\x1b[23m to begin'
-
 
 function fpath (f) {
   return path.join(__dirname, f)
 }
-
 
 workshopper({
     name        : name
