118ea8369a81ce7cc27c57a58f99a8ec1d07a284
Alan Shaw <alan@freestyle-developments.co.uk>
2014-05-23 12:39:39 +0100

    Fix background colour

--- nodebot-workshop.js
+++ nodebot-workshop.js
@@ -22,5 +22,5 @@ workshopper({
   , appDir      : __dirname
   , helpFile    : fpath('help.txt')
   , menuItems   : []
-  , menu        : {fg: "black", bg: "yellow"}
+  , menu        : {fg: "black", bg: 220}
 })
