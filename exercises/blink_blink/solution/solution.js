d462490075ad5507c08e4f7f36c7d7acb7f0fbb0
olizilla <oli@zilla.org.uk>
2014-05-23 13:00:14 +0100

    Accepted answer style should match boilerplate

--- exercises/blink_blink/solution/solution.js
+++ exercises/blink_blink/solution/solution.js
@@ -1,7 +1,7 @@
-var five = require('johnny-five'),
-  board = new five.Board();
+var five = require('johnny-five')
+var board = new five.Board()
 
-board.on('ready', function() {
-  var led = new five.Led(13);
+board.on('ready', function () {
+  var led = new five.Led(13)
   led.strobe(1000)
-});
+})
