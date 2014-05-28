49e7dbfecf4984964660030605caba90b7a8654f
Alan Shaw <alan@freestyle-developments.co.uk>
2014-05-28 08:28:39 +0100

    Linted ping bell solution

--- exercises/ping_bell/solution/solution.js
+++ exercises/ping_bell/solution/solution.js
@@ -7,8 +7,8 @@ board.on('ready', function () {
   var server = dgram.createSocket('udp4')
 
   server.on('message', function () {
-    piezo.song("cdfda ag cdfdg gf ", "111111442111111442");
-  });
+    piezo.song('cdfda ag cdfdg gf ', '111111442111111442')
+  })
 
   server.bind(1337)
-});
+})
